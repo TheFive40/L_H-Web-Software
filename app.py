7e06f86854584092e096fa0906991176f1ae2cb8
TheFive40 <jeanf2805@gmail.com>
2025-01-06 10:57:35 -0500

    Update 6/01/2025

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 
 from flask import Flask, render_template, flash, redirect, url_for, session
@@ -52,4 +53,5 @@ def unauthorized():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
